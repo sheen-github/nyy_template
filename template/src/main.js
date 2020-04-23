d5b56250f69342fd336b2f47b6a99d4e663ddc44
shengang <88274091@163.com>
2020-04-23 18:24:05 +0800

    asdf

--- template/src/main.js
+++ template/src/main.js
@@ -1,32 +1,17 @@
-{{#if_eq build "standalone"}}
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
 import Vue from 'vue'
 import App from './App'
-{{#router}}
 import router from './router'
-{{/router}}
-
-{{#vuex}}  
-//vuex为true的时候就会写入这些
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import store from  './store/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vuex}}
+import Vuex from 'vuex'
+import store from  './store/store'
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  {{#vuex}}
   store,
-  {{/vuex}}
-  {{#router}}
   router,
-  {{/router}}
   {{#if_eq build "runtime"}}
-  render: h => h(App)
+  render: h => h(App)，
   {{/if_eq}}
   {{#if_eq build "standalone"}}
   components: { App },
