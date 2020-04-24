eb81f4a309b1ee981bea2545fa2bd13ab9d60a98
shengang <88274091@163.com>
2020-04-24 12:19:51 +0800

    asdf

--- meta.js
+++ meta.js
@@ -11,7 +11,7 @@ const templateVersion = pkg.version
 const {
 	addTestAnswers
 } = require('./scenarios')
-console.log('欢迎使用诺亚云，组件模板')
+console.log('欢迎使用诺亚云，组件模板1111111')
 module.exports = {
 	metalsmith: {
 		// When running tests for the template, this adds answers for the selected scenario
